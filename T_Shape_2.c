bb80f4867df723c2273eac58e8ee2967775e610f
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-05-09 17:48:24 +0600

    pyramid shapre problem solved

---
+++ T_Shape_2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+  int n;
+  scanf("%d", &n);
+  for (int i = n-1; i >= 0; i--)
+  {
+    for (int j = 1; j <= i; j++)
+    {
+      printf(" ");
+    }
+    for (int k = 1; k <= n-i; k++) {
+      printf("*");
+    }
+    for (int l = 0; l < n-i-1; l++) {
+      printf("*");
+    }
+    printf("\n");
+  }
+  return 0;
+}
