ceca9194ed18c9f3d6888a55292b842950b52134
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-05-15 18:40:57 +0600

    again matrix problem solved

---
+++ Matrix_Again.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main () {
+  int n, m;
+  scanf("%d %d", &n, &m);
+  int arr[n][m];
+  for(int i = 0; i < n; i++) {
+    for(int j = 0; j < m; j++) {
+      scanf("%d", &arr[i][j]);
+    }
+  }
+  for(int i = n-1; i < n; i++) {
+    for(int j = 0; j < m; j++) {
+      printf("%d ", arr[i][j]);
+    }
+    printf("\n");
+  }
+  for(int i = 0; i < n; i++) {
+    for(int j = m-1; j < m; j++) {
+      printf("%d ", arr[i][j]);
+    }
+  }
+  return 0;
+}
