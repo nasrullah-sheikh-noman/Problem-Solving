7f572e9faae80380ea5056d811f8321d2e8956fc
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-05-18 20:06:40 +0600

    codechef dsa problem solved

---
+++ Chef_wants_to_buy_a_laptop.cpp
@@ -0,0 +1,6 @@
+int borrowMoney(int X, int Y) {
+    if(X <= Y) {
+        return 0;
+    } else return X - Y;
+    
+}
---
+++ Duplicate_Integers.cpp
@@ -0,0 +1,8 @@
+bool hasDuplicate(const vector<int>& nums) {
+    unordered_set<int> s;
+    for(auto x : nums) {
+        if(s.count(x)) return true;
+        s.insert(x);
+    }
+    return false;
+}
