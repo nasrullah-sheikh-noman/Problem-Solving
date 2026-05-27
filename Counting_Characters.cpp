3bd6ed9f0b346564991fdd056974e0056de9af05
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-05-27 20:51:53 +0600

    codechef contest one problem solved

---
+++ Counting_Characters.cpp
@@ -0,0 +1,37 @@
+// starting with the name of almighty ALLAH
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define vc vector
+#define vi vc<int>
+#define vl vc<ll>
+#define endl "\n" 
+#define yes cout << "YES" << endl; 
+#define no cout << "NO" << endl; 
+using namespace std;
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while(t--) {
+    int n;
+    cin >> n;
+    string s;
+    for(int i = 0; i < n; i++) {
+      cin >> s[i];
+    }
+    int a = 0, b=0;
+    for(int i = 0; i < n; i++) {
+      if(s[i] == 'a') {
+        a++;
+      } else if(s[i] == 'b') b++;
+    }
+    cout << a << " " << b << endl;
+  }
+
+  return 0;
+}
