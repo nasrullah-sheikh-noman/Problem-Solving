6785c5ab6ab8fdd6e392eed5982ff31b0f8e8a98
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-06-02 22:25:33 +0600

    two problem solved in vjudge

---
+++ Clear_Day.cpp
@@ -0,0 +1,22 @@
+// starting with the name of almighty ALLAH
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define vc vector
+#define vi vc<int>
+#define vl vc<ll>
+#define endl "\n" 
+#define yes cout << "YES" << endl; 
+#define no cout << "NO" << endl; 
+using namespace std;
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int x, y; cin >> x >> y;
+  cout << 7-x-y << endl;
+
+  return 0;
+}
