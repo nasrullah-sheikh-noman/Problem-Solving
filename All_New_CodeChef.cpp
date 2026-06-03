f3902f13fa550da56efb12c901746c3143240312
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-06-03 18:27:50 +0600

    codechef problem solved

---
+++ All_New_CodeChef.cpp
@@ -0,0 +1,24 @@
+// starting with the name of almighty ALLAH
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define vc vector
+#define vi vc<int>
+#define vl vc<ll>
+#define endl "\n" 
+#define yes cout << "YES" << endl; 
+#define no cout << "NO" << endl; 
+using namespace std;
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int a, b; cin >> a >> b;
+  if(a < b) cout << "Old\n";
+  else if(a > b) cout << "New\n";
+  else cout << "Same\n";
+
+  return 0;
+}
