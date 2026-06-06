412ccf87f9842cc5d7c3d7221baac9693dcbbd2d
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-06-06 19:55:52 +0600

    vjudge problem solved

---
+++ Fever.cpp
@@ -0,0 +1,27 @@
+// starting with the name of almighty ALLAH
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define vc vector
+#define vi vc<int>
+#define vl vc<ll>
+#define endl "\n" 
+#define yes cout << "YES" << endl; 
+#define no cout << "NO" << endl; 
+using namespace std;
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t; cin >> t;
+  while(t--) {
+    int n; cin >> n;
+    if(n > 98) {
+      cout << "YES\n";
+    } else cout << "NO\n";
+  }
+
+  return 0;
+}
