bd89125bf9b4e589f1fe8b09eadc7858d0d85b4d
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-06-06 20:06:59 +0600

    c problem solved in vjudge

---
+++ c.cpp
@@ -0,0 +1,27 @@
+// starting with the name of almighty ALLAH
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define vc vector
+#define vi vc<int>
+#define vl vc<ll>
+#define endl "\n" 
+#define yes cout << "YES" << endl; 
+#define no cout << "NO" << endl; 
+using namespace std;
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t; cin >> t;
+  while(t--) {
+    int n, m; cin >> n >> m;
+    if(m > n) {
+      cout << m-n << endl;
+    } else cout << "0\n";
+  }
+
+  return 0;
+}
