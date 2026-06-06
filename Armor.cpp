50d9cf8f82869b07d34d50397df0eda09dbb0951
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-06-06 18:34:49 +0600

    atcoder contest problem solved

---
+++ Armor.cpp
@@ -0,0 +1,23 @@
+// starting with the name of almighty ALLAH
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define vc vector
+#define vi vc<int>
+#define vl vc<ll>
+#define endl "\n" 
+#define yes cout << "YES" << endl; 
+#define no cout << "NO" << endl; 
+using namespace std;
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int a, b; cin >> a >> b;
+  if (a <= b) cout << "Yes\n";
+  else cout << "No\n";
+
+  return 0;
+}
