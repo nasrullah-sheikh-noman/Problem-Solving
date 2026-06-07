edf851aa2aa282e4df20325bfca427ff0630e49e
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-06-08 01:00:37 +0600

    practice switch

---
+++ switch.cpp
@@ -0,0 +1,32 @@
+// starting with the name of almighty ALLAH
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define vc vector
+#define vi vc<int>
+#define vl vc<ll>
+#define endl "\n" 
+#define yes cout << "YES" << endl; 
+#define no cout << "NO" << endl; 
+using namespace std;
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n = 4;
+  switch(n) 
+  {
+    case 1:
+      cout << "sat\n";
+    case 2:
+      cout << "sun\n";
+    case 3:
+      cout << "mon\n";
+    case 4:
+      cout << "wed\n";
+  }
+
+  return 0;
+}
