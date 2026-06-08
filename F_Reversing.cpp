9c10be6a5d09efa698b5299febc807a67b7b4289
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-06-08 15:33:42 +0600

    reverse porblem solved using swap func

--- F_Reversing.cpp
+++ F_Reversing.cpp
@@ -1,3 +1,31 @@
+// // starting with the name of almighty ALLAH
+// #include <bits/stdc++.h>
+// #define ll long long
+// #define pb push_back
+// #define vc vector
+// #define vi vc<int>
+// #define vl vc<ll>
+// #define endl "\n" 
+// #define yes cout << "YES" << endl; 
+// #define no cout << "NO" << endl; 
+// using namespace std;
+
+// int main()
+// {
+//   ios::sync_with_stdio(false);
+//   cin.tie(nullptr);
+
+//   int n; cin >> n;
+//   int arr[n+1];
+//   for(int i = 0; i < n; i++) {
+//     cin >> arr[i];
+//   }
+//   for(int i = n-1; i >= 0; i--) {
+//     cout << arr[i] << " ";
+//   }
+//   return 0;
+// }
+
 // starting with the name of almighty ALLAH
 #include <bits/stdc++.h>
 #define ll long long
@@ -16,11 +44,14 @@ int main()
   cin.tie(nullptr);
 
   int n; cin >> n;
-  int arr[n+1];
+  int arr[n];
   for(int i = 0; i < n; i++) {
     cin >> arr[i];
   }
-  for(int i = n-1; i >= 0; i--) {
+  for(int i = 0; i < n/2; i++) {
+    swap(arr[i], arr[n-1-i]);
+  }
+  for(int i = 0; i < n; i++) {
     cout << arr[i] << " ";
   }
   return 0;
