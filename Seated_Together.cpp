00290b5066c17af715c1cedfa16bab02496205c5
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-06-10 22:12:18 +0600

    seated together problem solved

---
+++ Seated_Together.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int n; cin >> n; 
+    if(n%5 != 0) {
+        cout << "Yes\n";
+    } else cout << "No\n";
+}
