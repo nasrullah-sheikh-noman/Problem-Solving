527afb6e6547fd92fdade72ade524d4985fa6df0
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-06-11 19:50:30 +0600

    palindrome problem solved

--- I_Palindrome.cpp
+++ I_Palindrome.cpp
@@ -15,7 +15,13 @@ int main()
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
-  // write your code here
-
+  string s; cin >> s;
+  for(int i = 0; i < s.size()/2; i++) {
+    if(s[i] != s[s.size()-1-i]) {
+      cout << "NO\n";
+      return 0;
+    }
+  }
+  cout << "YES\n";
   return 0;
 }
