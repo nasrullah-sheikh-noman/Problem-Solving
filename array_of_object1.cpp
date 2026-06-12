2535f583440c9095d9b70ccfe05ef9c7ba8c4370
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-06-12 17:07:39 +0600

    reverse array of object

---
+++ array_of_object1.cpp
@@ -0,0 +1,35 @@
+// starting with the name of almighty ALLAH
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define vc vector
+#define vi vc<int>
+#define vl vc<ll>
+#define endl "\n" 
+#define yes cout << "YES" << endl; 
+#define no cout << "NO" << endl; 
+using namespace std;
+
+class Student {
+  public :
+    string name;
+    int roll;
+    int marks;  
+};
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n; cin >> n;
+  Student arr[n];
+  for(int i = 0; i < n; i++) {
+    cin >> arr[i].name >> arr[i].roll >> arr[i].marks;
+  }
+  reverse(arr, arr+n);
+  for(int i = 0; i < n; i++) {
+    cout << arr[i].name << " " << arr[i].roll << " " << arr[i].marks << endl;
+  }
+  return 0;
+}
