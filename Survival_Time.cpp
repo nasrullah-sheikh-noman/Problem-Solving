9d8a45d7e390c8c47cc5572bfcb9ad9ed716110b
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-06-14 16:51:33 +0600

    survival time problem solved

---
+++ Survival_Time.cpp
@@ -0,0 +1,27 @@
+// starting with the name of almighty ALLAH
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define vc vector
+#define vi vc<int>
+#define vl vc<ll>
+#define endl "\n" 
+#define yes cout << "YES" << endl; 
+#define no cout << "NO" << endl; 
+using namespace std;
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t; cin >> t;
+  while(t--) {
+    int a, b, c; cin >> a >> b >> c;
+    int pd = b*5;
+    int deb = a/pd;
+    cout << deb+c << endl;
+  }
+
+  return 0;
+}
