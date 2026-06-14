074e319f9cce108e1556e9a6079027d6e4a3f9ac
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-06-14 09:23:26 +0600

    chess pairing problem solved

---
+++ Chess_Pairing.cpp
@@ -0,0 +1,28 @@
+// starting with the name of almighty ALLAH
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define vc vector
+#define vi vc<int>
+#define vl vc<ll>
+#define endl "\n" 
+#define yes cout << "YES" << endl; 
+#define no cout << "NO" << endl; 
+using namespace std;
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t; cin >> t;
+  while(t--) {
+    int n, x; cin >> n >> x;
+    int u = (2*n)-x;
+    if(u >= x) {
+      cout << "0\n";
+    } else cout << x - u << endl;
+  }
+
+  return 0;
+}
