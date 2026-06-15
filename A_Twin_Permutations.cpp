033548eb6c947b287d6e6a3d8c02298eeaaba523
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-06-15 13:20:56 +0600

    twin permutations problem solved

---
+++ A_Twin_Permutations.cpp
@@ -0,0 +1,34 @@
+// starting with the name of almighty ALLAH
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define vc vector
+#define vi vc<int>
+#define vl vc<ll>
+#define endl "\n" 
+#define yes cout << "YES" << endl; 
+#define no cout << "NO" << endl; 
+using namespace std;
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t; cin >> t;
+  while(t--) {
+    int n; cin >> n;
+
+      vi v(n), v2(n);
+      for(int i = 0; i < n; i++) {
+        cin >> v[i];
+        v2[i] = n+1 - v[i];
+      }
+      for(auto x : v2) {
+        cout << x << " ";
+      }
+    cout << endl;
+  }
+
+  return 0;
+}
