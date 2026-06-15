0d524e3b9110f69723947f8f44fd8cb138bf721e
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-06-15 20:18:50 +0600

    largest rectangle in histogram

---
+++ Largest_Rectangle_in_Histogram.cpp
@@ -0,0 +1,23 @@
+long long largestRectangleArea(int N, vector<int>& A) {
+    //write your code here
+    stack<int> st;
+    long long mxArea = 0;
+    for(int i = 0; i < N; i++) {
+        while(!st.empty() && A[st.top()] > A[i]) {
+            int h = A[st.top()];
+            st.pop();
+            int left = st.empty() ? -1 : st.top();
+            long long width = i - left - 1;
+            mxArea = max(mxArea, (long long)h * width);
+        }
+        st.push(i);
+    }
+    while(!st.empty()) {
+        int h = A[st.top()];
+        st.pop();
+        int left = st.empty() ? -1 : st.top();
+        long long width = N - left - 1;
+        mxArea = max(mxArea, (long long)h * width);
+    }
+    return mxArea;
+}
