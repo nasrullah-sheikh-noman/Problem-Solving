845c296658644f85d78c350501b62509a6672f9d
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-06-17 09:46:31 +0600

    loop problem solved

---
+++ for_loop.cpp
@@ -0,0 +1,30 @@
+// starting with the name of almighty ALLAH
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define vc vector
+#define vi vc<int>
+#define vl vc<ll>
+#define endl "\n" 
+#define yes cout << "YES" << endl; 
+#define no cout << "NO" << endl; 
+using namespace std;
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int x = 0, y = 2;
+  int cnt = 0;
+  for(int i = x; i <= y; i++) {
+    for(int j = i; j <= y; j++) {
+      for(int k = j; k <= y; k++) {
+        cout << i << " " << j << " " << k << endl;
+        cnt++;
+      }
+    }
+  }
+  cout << cnt << endl;
+  return 0;
+}
