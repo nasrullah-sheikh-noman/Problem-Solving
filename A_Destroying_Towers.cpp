402a7afbcd2bd47416ff03af28d5443974a363ec
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-06-18 22:01:09 +0600

    destroying towers problem solved

---
+++ A_Destroying_Towers.cpp
@@ -0,0 +1,32 @@
+// starting with the name of almighty ALLAH
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define vc vector
+#define vi vc<int>
+#define vl vc<ll>
+#define endl "\n" 
+#define yes cout << "YES" << endl; 
+#define no cout << "NO" << endl; 
+using namespace std;
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t; cin >> t;
+  while(t--) {
+    int n; cin >> n;
+    ll ans = 0;
+    int mn = INT_MAX;
+    for(int i = 1; i <= n; i++) {
+      int x; cin >> x;
+      mn = min(mn, x);
+      ans+=mn;
+    }
+    cout << ans << endl;
+  }
+
+  return 0;
+}
