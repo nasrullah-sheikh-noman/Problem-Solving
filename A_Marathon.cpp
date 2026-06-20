6bdfeb6649faf703c99ed7794fbe248d8ab487a6
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-06-20 09:00:04 +0600

    marathon problem solved

---
+++ A_Marathon.cpp
@@ -0,0 +1,29 @@
+// starting with the name of almighty ALLAH
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define vc vector
+#define vi vc<int>
+#define vl vc<ll>
+#define endl "\n" 
+#define yes cout << "YES" << endl; 
+#define no cout << "NO" << endl; 
+using namespace std;
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n; cin >> n;
+  while(n--) {
+    int a, b, c, d; cin >> a >> b >> c >> d;
+    int cnt = 0;
+    if(b > a) cnt++;
+    if(c > a) cnt++;
+    if(d > a) cnt++;
+    cout << cnt << endl;
+  }
+
+  return 0;
+}
