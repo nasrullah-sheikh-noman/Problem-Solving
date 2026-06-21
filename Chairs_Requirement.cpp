0979739427844ef1e509778bc845333f89382949
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-06-21 11:31:15 +0600

    chairs requirement problem solved

---
+++ Chairs_Requirement.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t; cin >> t;
+    while(t--) {
+        int x, y; cin >> x >> y;
+        if(x < y) cout << "0\n";
+        else cout << x-y << endl;
+    }
+}
