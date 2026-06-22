0b3a2a8c8f9a08a89bbd04cce21f83f92f415066
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-06-22 11:41:02 +0600

    multi test cases problem solved

---
+++ Multi_Test_Cases.cpp
@@ -0,0 +1,33 @@
+// starting with the name of almighty ALLAH
+#include <bits/stdc++.h>
+#define ll long long
+#define pb(x) push_back(x)
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)x.size()
+#define szl(x) (ll)x.size()
+#define vc vector
+#define vi vc<int>
+#define vl vc<ll>
+#define endl "\n" 
+#define yes cout << "YES" << endl; 
+#define no cout << "NO" << endl; 
+using namespace std;
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t; cin >> t;
+  while(t--) {
+    int n; cin >> n;
+    int cnt = 0;
+    for(int i = 0; i < n; i++) {
+      int x; cin >> x;
+      if(x%2 == 1) cnt++;
+    }
+    cout << cnt << endl;
+  }
+
+  return 0;
+}
