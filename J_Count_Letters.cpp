082d020f9e043a26b557dc65a9bf0566045cf1b3
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-06-23 13:35:26 +0600

    count letters problem solved

---
+++ J_Count_Letters.cpp
@@ -0,0 +1,32 @@
+// starting with the name of almighty ALLAH
+#include <bits/stdc++.h>
+#define ll long long
+#define pb(x) push_back(x)
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)x.size()
+#define szl(x) (ll)x.size()
+#define vc vector
+#define vi vc<int>
+#define vl vc<ll>
+#define endl "\n" 
+#define yes cout << "YES" << endl; 
+#define no cout << "NO" << endl; 
+using namespace std;
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s; cin >> s;
+  vi v(26, 0);
+  for(int i = 0; i < s.size(); i++) {
+    v[s[i]-'a']++;
+  }
+  for(int i = 0; i < 26; i++) {
+    if(v[i]>0) {
+      cout << char(i+'a') << " : " << v[i] << endl;
+    }
+  }
+  return 0;
+}
