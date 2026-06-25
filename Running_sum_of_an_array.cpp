4f945fe6b995c7059fab78f38a465e42b8878e64
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-06-25 12:09:11 +0600

    running sum of an aray problem solve using vector, func & prefix sum

---
+++ Running_sum_of_an_array.cpp
@@ -0,0 +1,41 @@
+// starting with the name of almighty ALLAH
+#include <bits/stdc++.h>
+#define ll long long
+#define pb(x) push_back(x)
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)x.size()
+#define szl(x) (ll)x.size()
+#define vc vector
+#define vi vc<int>
+#define vl vc<ll>
+#define endl "\n" 
+#define yes cout << "YES" << endl; 
+#define no cout << "NO" << endl; 
+using namespace std;
+
+vector<int> Run(const vi& v) {
+  int n = v.size();
+  vi pre(n);
+  pre[0]= v[0];
+  for(int i = 1; i < n; i++) {
+    pre[i] = pre[i-1]+v[i];
+  }
+  return pre;
+}
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n; cin >> n;
+  vi v(n);
+  for(int i = 0; i < n; i++) {
+    cin >> v[i];
+  }
+  vi ans = Run(v);
+  for(int i = 0; i < n; i++) {
+    cout << ans[i] << " ";
+  }
+  return 0;
+}
