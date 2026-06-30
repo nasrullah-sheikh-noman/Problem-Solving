d7ba0b01770ebfbdc28e8a539d94c5e13aade8c5
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-06-30 19:24:36 +0600

    painting walls problem solved

---
+++ Painting_Walls.cpp
@@ -0,0 +1,33 @@
+// starting with the name of almighty ALLAH
+#include <bits/stdc++.h>
+#define ll long long
+#define i8 __int128_t
+#define ui8 __uint128_t
+#define pb(x) push_back(x)
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)x.size()
+#define szl(x) (ll)x.size()
+#define vc vector
+#define vi vc<int>
+#define vl vc<ll>
+#define ul unsigned ll
+#define vu vc<ul>
+#define endl "\n" 
+#define yes cout << "YES" << endl; 
+#define no cout << "NO" << endl; 
+using namespace std;
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t; cin >> t;
+  while(t--) {
+    int x, y, z; cin >> x >> y >> z;
+    int cost = (x*y)*2;
+    cout << z/cost << endl;
+  }
+
+  return 0;
+}
