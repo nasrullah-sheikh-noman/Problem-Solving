593abb60662ae5c0fa74a6c5d1b8fba019465f97
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-06-30 13:10:29 +0600

    insert at head in linked list

---
+++ insert_at_head_in_linked_list.cpp
@@ -0,0 +1,63 @@
+// starting with the name of almighty ALLAH
+#include <bits/stdc++.h>
+#define ll long long
+#define i8 __int128_t
+#define ui8 __uint128_t
+#define pb(x) push_back(x)
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)x.size()
+#define szl(x) (ll)x.size()
+#define vc vector
+#define vi vc<int>
+#define vl vc<ll>
+#define ul unsigned ll
+#define vu vc<ul>
+#define endl "\n" 
+#define yes cout << "YES" << endl; 
+#define no cout << "NO" << endl; 
+using namespace std;
+
+class Node {
+  public:
+    int val;
+    Node* next;
+  Node(int val) {
+    this->val = val;
+    this->next = NULL;
+  }
+};
+
+void insert_at_head(Node* &head, int val) {
+  Node* newNode = new Node(val);
+  newNode->next = head;
+  head = newNode;
+}
+
+void print_linked_list(Node* head) {
+  Node* tmp = head;
+  while(tmp!=NULL) {
+    cout << tmp->val << endl;
+    tmp = tmp->next;
+  }
+}
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  Node* head = new Node(10);
+  Node* a = new Node(20);
+  Node* b = new Node(30);
+
+  head->next = a;
+  a->next = b;
+
+  insert_at_head(head, 40);
+  insert_at_head(head, 50);
+  insert_at_head(head, 60);
+  
+  print_linked_list(head);
+
+  return 0;
+}
