5402a4cfe1d40ff82f8fe38c6bde81c13a98140c
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-07-01 15:46:51 +0600

    rotate problem solved in twoD array

--- ASCII_Art.cpp
+++ ASCII_Art.cpp
@@ -22,20 +22,17 @@ int main()
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
-  int n; cin >> n;
-  vc<string> v(n);
+  int n, m; cin >> n >> m;
+  vc<vc<int>> v(n, vi(m));
   for(int i = 0; i < n; i++) {
-    cin >> v[i];
+    for(int j = 0; j < m; j++) {
+      cin >> v[i][j];
+    }
   }
-  vc<string> v1 = v;
-  for(int i = 1; i < n; i++) v1[0][i] = v[0][i-1];
-  for(int i = 1; i < n; i++) v1[i][n-1] = v[i-1][n-1];
-  for(int i = n-2; i >= 0; i--) v1[n-1][i] = v[n-1][i+1];
-  for(int i = n-2; i >= 0; i--) v1[i][0] = v[i+1][0];
-
   for(int i = 0; i < n; i++) {
-    for(int j = 0; j < n; j++) {
-      cout << v1[i][j];
+    for(int j = 0; j < m; j++) {
+      if(v[i][j] == 0) cout << ".";
+      else cout << (char)(v[i][j]+64);
     }
     cout << endl;
   }
