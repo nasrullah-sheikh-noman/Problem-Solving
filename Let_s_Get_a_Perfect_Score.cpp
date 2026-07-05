103670a5c0215ee58afff81ebc05eb25a4d8352c
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-07-05 21:27:17 +0600

    cinema problem solved in basecamp

---
+++ Let_s_Get_a_Perfect_Score.cpp
@@ -0,0 +1,80 @@
+// Starting with the name of Almighty Allah
+// Practice in the only shortcut to improve
+#include <bits/stdc++.h>
+#define ll long long
+#define i8 __int128_t
+#define ui8 __uint128_t
+#define pb(x) push_back(x)
+#define pf(x) push_front(x)
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
+#define rvs(v) reverse(v.begin(), v.end())
+#define sz(x) (int)x.size()
+#define szl(x) (ll)x.size()
+#define F first
+#define S second
+#define nl '\m'
+#define cinv(v) for(auto &x: v) cin >> x;
+#define coutvl(v) for(auto &x: v) cout << x << nl;
+#define coutv(v) for(auto &x: v) cout << x << " "; cout << nl; 
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define mp(x, y) make_pair(x, y)
+#define vc vector
+#define vi vc<int>
+#define vl vc<ll>
+#define ld long double
+#define ul unsigned ll
+#define vu vc<ul>
+#define mod 1000000007
+#define gcd(a, b) __gcd(a, b)
+#define lcd(a, b) (a * b) / gcd(a, b)
+#define PI 2 * acos(0.0)
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define yes cout << "Yes" << nl; 
+#define Yes cout << "YES" << nl; 
+#define no cout << "No" << nl; 
+#define No cout << "NO" << nl; 
+const int N = 1e5;
+using namespace std;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int my_rand(int l, int r) {
+  return uniform_int_distribution<int>(l, r)(rng);
+}
+
+void solve() {
+  int n, m; cin >> n >> m;
+  vc<string> v(n);
+  cinv(v);
+  int cnt = 0;
+  for(int i = 0; i < n; i++) {
+    for(int j = i+1; j < n; j++) {
+      bool ok = true;
+      for(int k = 0; k < m; k++) {
+        if(v[i][k] == 'x' && v[j][k] == 'x') {
+          ok = false;
+        }
+      }
+      if(ok) cnt++;
+    }
+  }
+  cout << cnt << endl;
+}
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t = 1;
+  // cin >> t;
+  while(t--) {
+  // cout << "Case : " << t << " ";
+    solve();
+ 
+  }
+
+  return 0;
+}
