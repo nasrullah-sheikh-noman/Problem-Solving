d64e8409319666360aceaaed777182753bbdf492
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-07-09 12:57:23 +0600

    jellyfish & game problem solved

---
+++ A_Jellyfish_and_Game.cpp
@@ -0,0 +1,97 @@
+// Starting with the name of Almighty Allah
+// Practice in the only shortcut to improve
+#include <bits/stdc++.h>
+#define ll long long
+#define i8 __int128_t
+#define ui8 __uint128_t
+#define pb(x) push_back(x)
+#define pf(x) push_front(x)
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
+#define rvs(v) reverse(v.begin(), v.end())
+#define sz(x) (int)x.size()
+#define szl(x) (ll)x.size()
+#define F first
+#define S second
+#define nl '\n'
+#define cinv(v) for(auto &x: v) cin >> x;
+#define coutvl(v) for(auto &x: v) cout << x << nl;
+#define coutv(v) for(auto &x: v) cout << x << " "; cout << nl; 
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define mp(x, y) make_pair(x, y)
+#define vc vector
+#define vi vc<int>
+#define vl vc<ll>
+#define ld long double
+#define ul unsigned ll
+#define vu vc<ul>
+#define mod 1000000007
+#define gcd(a, b) __gcd(a, b)
+#define lcd(a, b) (a * b) / gcd(a, b)
+#define PI 2 * acos(0.0)
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define yes cout << "Yes" << nl; 
+#define Yes cout << "YES" << nl; 
+#define no cout << "No" << nl; 
+#define No cout << "NO" << nl; 
+const int N = 1e5;
+using namespace std;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int my_rand(int l, int r) {
+  return uniform_int_distribution<int>(l, r)(rng);
+}
+
+void solve() {
+  int n, m; ll k; cin >> n >> m >> k;
+  vl v1(n);
+  vl v2(m);
+  cinv(v1);
+  cinv(v2);
+  ll sum = 0;
+  for(auto x: v1) sum+=x;
+  int iv1 = min_element(v1.begin(), v1.end()) - v1.begin();
+  int iv2 = max_element(v2.begin(), v2.end()) - v2.begin();
+  if(v1[iv1] < v2[iv2]) {
+    sum += v2[iv2] - v1[iv1];
+    swap(v1[iv1], v2[iv2]);
+  }
+  if(k==1) {
+    cout << sum << nl;
+    return;
+  }
+  iv1 = max_element(v1.begin(), v1.end()) - v1.begin();
+  iv2 = min_element(v2.begin(), v2.end()) - v2.begin();
+  if(v1[iv1] > v2[iv2]) {
+    sum-=v1[iv1]-v2[iv2];
+    swap(v1[iv1], v2[iv2]);
+  }
+  if(k%2==0) {
+    cout << sum << nl;
+  } else {
+    int iv1 = min_element(v1.begin(), v1.end()) - v1.begin();
+    int iv2 = max_element(v2.begin(), v2.end()) - v2.begin();
+    if(v1[iv1] < v2[iv2]) {
+      sum += v2[iv2]-v1[iv1];
+    }
+    cout << sum << nl;
+  }
+}
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t = 1;
+  cin >> t;
+  while(t--) {
+  // cout << "Case : " << t << " ";
+    solve();
+ 
+  }
+
+  return 0;
+}
