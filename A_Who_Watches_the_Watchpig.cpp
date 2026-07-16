e070207d623cd33ec81b9c66fc3bea05c45e477d
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-07-16 21:50:51 +0600

    two problem solved in codeforces contest

---
+++ A_Who_Watches_the_Watchpig.cpp
@@ -0,0 +1,125 @@
+// Starting with the name of Almighty Allah
+// Practice is the only shortcut to improve
+
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+
+//==================== TYPE ALIASES ====================//
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using i8 = __int128_t;
+using ui8 = __uint128_t;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+template<typename T>
+using vc = vector<T>;
+
+using vi = vc<int>;
+using vl = vc<ll>;
+using vpi = vc<pii>;
+using vpl = vc<pll>;
+
+//==================== CONSTANTS ====================//
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1000000007;
+const ld PI = acosl(-1.0L);
+
+//==================== MACROS ====================//
+#define pb push_back
+#define eb emplace_back
+#define F first
+#define S second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define srt(v) sort(all(v))
+#define rsrt(v) sort(rall(v))
+#define rvs(v) reverse(all(v))
+#define sz(x) (int)((x).size())
+#define nl '\n'
+#define cinv(v) for(auto &x : (v)) cin >> x
+#define coutv(v) for(auto &x : (v)) cout << x << ' '; cout << nl
+#define coutvl(v) for(auto &x : (v)) cout << x << nl
+#define Yes cout << "YES" << nl
+#define No cout << "NO" << nl
+#define yes cout << "Yes" << nl
+#define no cout << "No" << nl
+
+//==================== FUNCTIONS ====================//
+template<typename T>
+T gcd(T a, T b) {
+  while (b) {
+    T t = a % b;
+    a = b;
+    b = t;
+  }
+  return a;
+}
+
+template<typename T>
+T lcm(T a, T b) {
+  return a / gcd(a, b) * b;
+}
+
+template<typename T>
+bool ckmin(T &a, T b) {
+  if (b < a) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+template<typename T>
+bool ckmax(T &a, T b) {
+  if (b > a) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+//==================== RANDOM ====================//
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+int my_rand(int l, int r) {
+  return uniform_int_distribution<int>(l, r)(rng);
+}
+
+void solve() {
+  int n, k; cin >> n >> k;
+  string s;
+  cin >> s;
+  if(2*k > n) {
+    cout << -1 << nl;
+    return;
+  }
+  int mx = 0;
+    for (int i = 0; i < k; i++) {
+      if(s[i]=='L')
+        mx++;
+    }
+    for (int i = n-k; i < n; i++) {
+      if(s[i]=='R')
+        mx++;
+    }
+  cout << mx << nl;
+}
+
+int32_t main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t = 1;
+  cin >> t;
+
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}
