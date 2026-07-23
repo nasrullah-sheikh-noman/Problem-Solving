25db1594f30e942550c52cf7906fb3ef60d4ea42
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-07-23 13:51:25 +0600

    maximum equal stack sum problme solved in coding ninjas

---
+++ maximum_equal_stack_sum.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h> 
+  int fnc(stack<int> st) {
+    int sum = 0;
+    while(!st.empty()) {
+        sum+=st.top();
+        st.pop();
+    }
+    return sum;
+  }
+int maxSum(stack<int> &stk1, stack<int> &stk2, stack<int> &stk3) {
+    // Write your code here
+    int sum1 = fnc(stk1);
+    int sum2 = fnc(stk2);
+    int sum3 = fnc(stk3);
+    while(true) {
+        if(sum1==sum2 && sum2==sum3) break;
+        if(sum1>=sum2 && sum1>=sum3) {
+            sum1-=stk1.top();
+            stk1.pop();
+        }
+        else if(sum2>=sum1 && sum2>=sum3) {
+            sum2-=stk2.top();
+            stk2.pop();
+        }
+        else if(sum3>=sum2 && sum3>=sum1) {
+            sum3-=stk3.top();
+            stk3.pop();
+        }
+    }
+    return sum1;
+}
