082fbef1746ff88e68bde02cee69f5df56d81f54
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-07-24 13:00:49 +0600

    sharky surfing problem solved

---
+++ D_Sharky_Surfing.cpp
@@ -0,0 +1,143 @@
+// Starting with the name of Almighty Allah
+// Practice is the only shortcut to improve
+
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+
+//==================== TYPE ALIASES ====================//
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using i8 = __int128_t;
+using ui8 = __uint128_t;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+template<typename T>
+using vc = vector<T>;
+
+using vi = vc<int>;
+using vl = vc<ll>;
+using vpi = vc<pii>;
+using vpl = vc<pll>;
+
+//==================== CONSTANTS ====================//
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1000000007;
+const ld PI = acosl(-1.0L);
+
+//==================== MACROS ====================//
+#define pb push_back
+#define eb emplace_back
+#define F first
+#define S second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define srt(v) sort(all(v))
+#define rsrt(v) sort(rall(v))
+#define rvs(v) reverse(all(v))
+#define sz(x) (int)((x).size())
+#define nl '\n'
+#define cinv(v) for(auto &x : (v)) cin >> x
+#define coutv(v) for(auto &x : (v)) cout << x << ' '; cout << nl
+#define coutvl(v) for(auto &x : (v)) cout << x << nl
+#define Yes cout << "YES" << nl
+#define No cout << "NO" << nl
+#define yes cout << "Yes" << nl
+#define no cout << "No" << nl
+
+//==================== FUNCTIONS ====================//
+template<typename T>
+T gcd(T a, T b) {
+  while (b) {
+    T t = a % b;
+    a = b;
+    b = t;
+  }
+  return a;
+}
+
+template<typename T>
+T lcm(T a, T b) {
+  return a / gcd(a, b) * b;
+}
+
+template<typename T>
+bool ckmin(T &a, T b) {
+  if (b < a) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+template<typename T>
+bool ckmax(T &a, T b) {
+  if (b > a) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+//==================== RANDOM ====================//
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+int my_rand(int l, int r) {
+  return uniform_int_distribution<int>(l, r)(rng);
+}
+
+void solve() {
+  int n, m, L;
+  cin >> n >> m >> L;
+  int ans = 0, pow = 1;
+  vc<pair<pii, int>> a;
+  for (int i = 0; i < n; i++) {
+    int l, r;
+    cin >> l >> r;
+    a.pb({{l, r}, 1});
+  }
+  for (int i = 0; i < m; i++) {
+    int x, v;
+    cin >> x >> v;
+    a.pb({{x, v}, 0});
+  }
+  sort(a.begin(), a.end());
+  multiset<int, greater<int>> st;
+  for(auto [p,c]: a) {
+    if(c==0) {
+      st.insert(p.second);
+    } else {
+      int j = p.second - p.first + 2;
+      while(pow<j) {
+        if(st.empty()) {
+          ans = -1;
+          break;
+        } else {
+          pow += *st.begin();
+          st.erase(st.begin());
+          ans++;
+        }
+      }
+    }
+    if(ans==-1) break;
+  }
+  cout << ans << nl;
+}
+
+int32_t main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t = 1;
+  cin >> t;
+
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}
