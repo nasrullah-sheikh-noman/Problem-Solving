39ce302985202b581da02937ed4f00cbfafdccd5
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-08-01 08:43:38 +0600

    input binary tree

--- binary_tree_input.cpp
+++ binary_tree_input.cpp
@@ -93,41 +93,73 @@ int my_rand(int l, int r) {
 class Node {
   public: 
     int val;
-    Node* left;
-    Node* right;
-  Node(int val) {
-    this->val = val;
-    this->left = NULL;
-    this->right = NULL;
-  }
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
+      this->val = val;
+      this->left = NULL;
+      this->right = NULL;
+    }
 };
 
+Node* input_value() {
+  int val;
+  cin >> val;
+  Node *root;
+  if(val==-1)
+    root = NULL;
+  else root = new Node(val);
+  queue<Node *> q;
+  if(root) q.push(root);
+  while(!q.empty()) {
+    // 1. To extract
+    Node* p = q.front();
+    q.pop();
+
+    // 2. Working with nodes
+    int l, r;
+    cin >> l >> r;
+    Node *myLeft, *myRight;
+    if(l==-1)
+      myLeft = NULL;
+    else
+      myLeft = new Node(l);
+    if(r==-1)
+      myRight = NULL;
+    else
+      myRight = new Node(r);
+    p->left = myLeft;
+    p->right = myRight;
+
+    // 3. Pushing children
+    if(p->left)
+      q.push(p->left);
+    if(p->right)
+      q.push(p->right);
+  }
+  return root;
+}
+
 void level_order(Node* root) {
+  if(root==NULL) { 
+    cout << "No tree";
+    return;
+  }
   queue<Node *> q;
   q.push(root);
   while(!q.empty()) {
-    Node* p = q.front();
+    Node *p = q.front();
     q.pop();
     cout << p->val << " ";
     if(p->left) q.push(p->left);
-    if(p->right) q.push(p->right);
+    if(p->right)
+      q.push(p->right);
   }
 }
 
 void solve() {
-  Node* root = new Node(10);
-  Node *a = new Node(20);
-  Node *b = new Node(30);
-  Node *c = new Node(40);
-  Node *d = new Node(50);
-  Node *e = new Node(60);
-
-  root->left = a;
-  root->right = b;
-  a->left = c;
-  b->left = d;
-  b->right = e;
-
+  Node* root = input_value();
   level_order(root);
 }
 
