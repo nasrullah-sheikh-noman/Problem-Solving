0afd10b5dc31265b6995a91ab4ebfef00641e4d6
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-08-05 10:49:47 +0600

    N Queens problem solved

---
+++ N_queens.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool isSafe(vector<string> &board, int row, int col, int n) {
+      for (int i = 0; i < n; i++) {
+        if(board[row][i]=='Q')
+          return false;
+      }
+      for (int i = 0; i < n; i++) {
+        if(board[i][col]=='Q')
+          return false;
+      }
+      for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+        if(board[i][j]=='Q')
+          return false;
+      }
+      for (int i = row, j = col; i >= 0 && j < n; i--, j++) {
+        if(board[i][j]=='Q')
+          return false;
+      }
+      return true;
+    }
+
+    void NQueens(vector<string> board, vector<vector<string>> &ans, int row, int n) {
+      if(row==n) {
+        ans.push_back(board);
+        return;
+      }
+
+      for (int i = 0; i < n; i++) {
+        if(isSafe(board, row, i, n)) {
+          board[row][i] = 'Q';
+          NQueens(board, ans, row + 1, n);
+          board[row][i] = '.';
+        }
+      }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+      vector<string> board(n, string(n, '.'));
+      vector < vector<string>> ans;
+      NQueens(board, ans, 0, n);
+      return ans;
+    }
+};
