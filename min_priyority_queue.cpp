69a24e8faf6085b59659e7a04149c565f69b05e6
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-08-08 12:00:30 +0600

    min priority queue practice

--- min_priyority_queue.cpp
+++ min_priyority_queue.cpp
@@ -15,7 +15,7 @@ using ui8 = __uint128_t;
 using pii = pair<int, int>;
 using pll = pair<ll, ll>;
 
-template <typename T>
+template<typename T>
 using vc = vector<T>;
 
 using vi = vc<int>;
@@ -41,27 +41,18 @@ const ld PI = acosl(-1.0L);
 #define rvs(v) reverse(all(v))
 #define sz(x) (int)((x).size())
 #define nl '\n'
-#define cinv(v)       \
-  for (auto &x : (v)) \
-  cin >> x
-#define coutv(v)      \
-  for (auto &x : (v)) \
-    cout << x << ' '; \
-  cout << nl
-#define coutvl(v)     \
-  for (auto &x : (v)) \
-  cout << x << nl
+#define cinv(v) for(auto &x : (v)) cin >> x
+#define coutv(v) for(auto &x : (v)) cout << x << ' '; cout << nl
+#define coutvl(v) for(auto &x : (v)) cout << x << nl
 #define Yes cout << "YES" << nl
 #define No cout << "NO" << nl
 #define yes cout << "Yes" << nl
 #define no cout << "No" << nl
 
 //==================== FUNCTIONS ====================//
-template <typename T>
-T gcd(T a, T b)
-{
-  while (b)
-  {
+template<typename T>
+T gcd(T a, T b) {
+  while (b) {
     T t = a % b;
     a = b;
     b = t;
@@ -69,28 +60,23 @@ T gcd(T a, T b)
   return a;
 }
 
-template <typename T>
-T lcm(T a, T b)
-{
+template<typename T>
+T lcm(T a, T b) {
   return a / gcd(a, b) * b;
 }
 
-template <typename T>
-bool ckmin(T &a, T b)
-{
-  if (b < a)
-  {
+template<typename T>
+bool ckmin(T &a, T b) {
+  if (b < a) {
     a = b;
     return true;
   }
   return false;
 }
 
-template <typename T>
-bool ckmax(T &a, T b)
-{
-  if (b > a)
-  {
+template<typename T>
+bool ckmax(T &a, T b) {
+  if (b > a) {
     a = b;
     return true;
   }
@@ -100,41 +86,32 @@ bool ckmax(T &a, T b)
 //==================== RANDOM ====================//
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
-int my_rand(int l, int r)
-{
+int my_rand(int l, int r) {
   return uniform_int_distribution<int>(l, r)(rng);
 }
 
-void solve()
-{
-  priority_queue<int> pq;
+void solve() {
+  priority_queue<int, vi, greater<int>> pq;
+  pq.push(20);
   pq.push(10);
   pq.push(5);
-  pq.push(30);
-  pq.push(20);
+  pq.push(40);
   cout << pq.top() << nl;
-  cout << pq.size() << nl;
   pq.pop();
   cout << pq.top() << nl;
-  cout << pq.size() << nl;
-  if(pq.empty())
-    cout << "faka\n";
-  else
-    cout << "value ase\n";
-  pq.emplace(50);
+  pq.emplace(0);
   cout << pq.top() << nl;
+  
 }
 
-int32_t main()
-{
+int32_t main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
   int t = 1;
   // cin >> t;
 
-  while (t--)
-  {
+  while (t--) {
     solve();
   }
 
