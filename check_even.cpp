6e8ccbc3d22d4d2980e4ffd84aaac89a9a8d4d82
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-08-13 00:00:24 +0600

    check even problem solved

---
+++ check_even.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int l, r; cin >> l >> r;
+    if(l%2==0 || l < r) {
+        cout << "Yes\n";
+    } else cout << "No\n";
+}
