7b96ac6da4c40b00c24489363c22a100c29b2072
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-08-14 11:12:20 +0600

    previous smaller elements problem solved in greekforgreeks

--- previous_smaller_element.cpp
+++ previous_smaller_element.cpp
@@ -1,13 +1,19 @@
-vector<int> ans(arr.size(), 0);
-stack<int> st;
-for (int i = 0; i < arr.size(); i++) {
-  while(st.size() > 0 && st.top() >= arr[i]) {
-    st.pop();
-  }
-  if(st.empty())
-    ans[i] = -1;
-  else
-    ans[i] = st.top();
-  st.push(arr[i]);
-}
-return ans;
+class Solution {
+  public:
+    vector<int> prevSmaller(vector<int>& arr) {
+        //  code here
+        vector<int> ans(arr.size(), 0);
+        stack<int> st;
+        for (int i = 0; i < arr.size(); i++) {
+          while(st.size() > 0 && st.top() >= arr[i]) {
+            st.pop();
+          }
+          if(st.empty())
+            ans[i] = -1;
+          else
+            ans[i] = st.top();
+          st.push(arr[i]);
+        }
+        return ans;
+    }
+};
