b7ef000e8d74293e8fa99938516314ee94a20374
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-08-14 11:10:11 +0600

    reverse first k of a queue problem solved in greekforgreeks

---
+++ previous_smaller_element.cpp
@@ -0,0 +1,13 @@
+vector<int> ans(arr.size(), 0);
+stack<int> st;
+for (int i = 0; i < arr.size(); i++) {
+  while(st.size() > 0 && st.top() >= arr[i]) {
+    st.pop();
+  }
+  if(st.empty())
+    ans[i] = -1;
+  else
+    ans[i] = st.top();
+  st.push(arr[i]);
+}
+return ans;
--- reverse_first_k_of_a_queue.cpp
+++ reverse_first_k_of_a_queue.cpp
@@ -3,20 +3,26 @@ class Solution {
     queue<int> reverseFirstK(queue<int> q, int k) {
         // code here
         int n = q.size();
-        if(k<n) return q;
+        if(k>n) return q;
         stack<int> st;
-        while(k--) {
+        queue<int> tmp = q;
+        int h = k;
+        while(h--) {
             st.push(q.front());
             q.pop();
         }
         queue<int> ans;
-        while(st) {
+        while(!st.empty()) {
             ans.push(st.top());
             st.pop();
         }
-        while(q) {
-            ans.push(q.front());
-            q.pop();
+        int l = 1;
+        while(!tmp.empty()) {
+            if(l>k) {
+              ans.push(tmp.front());
+              tmp.pop();
+            } else tmp.pop();
+            l++;
         }
         return ans;
     }
