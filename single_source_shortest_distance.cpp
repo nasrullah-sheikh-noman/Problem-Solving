5935f266a5c57b497014117cf8873e24355b1cbb
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-08-18 18:34:37 +0600

    single source shortest distance practice

---
+++ single_source_shortest_distance.cpp
@@ -0,0 +1,144 @@
+// Starting with the name of Almighty Allah
+// Practice is the only shortcut to improve
+
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+
+//==================== TYPE ALIASES ====================//
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using i8 = __int128_t;
+using ui8 = __uint128_t;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+template<typename T>
+using vc = vector<T>;
+
+using vi = vc<int>;
+using vl = vc<ll>;
+using vpi = vc<pii>;
+using vpl = vc<pll>;
+
+//==================== CONSTANTS ====================//
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1000000007;
+const ld PI = acosl(-1.0L);
+
+//==================== MACROS ====================//
+#define pb push_back
+#define eb emplace_back
+#define F first
+#define S second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define srt(v) sort(all(v))
+#define rsrt(v) sort(rall(v))
+#define rvs(v) reverse(all(v))
+#define sz(x) (int)((x).size())
+#define nl '\n'
+#define cinv(v) for(auto &x : (v)) cin >> x
+#define coutv(v) for(auto &x : (v)) cout << x << ' '; cout << nl
+#define coutvl(v) for(auto &x : (v)) cout << x << nl
+#define Yes cout << "YES" << nl
+#define No cout << "NO" << nl
+#define yes cout << "Yes" << nl
+#define no cout << "No" << nl
+
+//==================== FUNCTIONS ====================//
+template<typename T>
+T gcd(T a, T b) {
+  while (b) {
+    T t = a % b;
+    a = b;
+    b = t;
+  }
+  return a;
+}
+
+template<typename T>
+T lcm(T a, T b) {
+  return a / gcd(a, b) * b;
+}
+
+template<typename T>
+bool ckmin(T &a, T b) {
+  if (b < a) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+template<typename T>
+bool ckmax(T &a, T b) {
+  if (b > a) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+//==================== RANDOM ====================//
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+int my_rand(int l, int r) {
+  return uniform_int_distribution<int>(l, r)(rng);
+}
+
+vector<int> adj_list[10001];
+bool visited[10001];
+int level[10001];
+
+void BFS(int src) {
+  queue<int> q;
+  q.push(src);
+  visited[src] = true;
+  level[src] = 0;
+  while(!q.empty()) {
+    int par = q.front();
+    q.pop();
+    for(auto x: adj_list[par]) {
+      if(!visited[x]) {
+        q.push(x);
+        visited[x] = true;
+        level[x] = level[par]+1;
+      }
+    }
+  }
+}
+
+void solve() {
+  int n, e;
+  cin >> n >> e;
+  while(e--) {
+    int a, b;
+    cin >> a >> b;
+    adj_list[a].push_back(b);
+    adj_list[b].push_back(a);
+  }
+  memset(visited, false, sizeof(visited));
+  memset(level, -1, sizeof(level));
+  int src, dst;
+  cin >> src >> dst;
+  BFS(src);
+  cout << "sortest distance -> " << level[dst];
+}
+
+int32_t main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t = 1;
+  // cin >> t;
+
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}
