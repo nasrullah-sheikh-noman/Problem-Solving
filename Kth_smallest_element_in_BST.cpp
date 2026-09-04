3cbf3bbdc13b1452e24f144b25595b16a7403b1c
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-09-04 10:27:15 +0600

    morning problem solved

--- Kth_smallest_element_in_BST.cpp
+++ Kth_smallest_element_in_BST.cpp
@@ -1,18 +1,35 @@
-int prev = 0;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int prev = 0;
 
-if(root==NULL)
-  return -1;
-if(root->left) {
-  int leftAns = KthSmallest(root->left, k);
-  if(leftAns)
-    return leftAns;
-}
-if(prev+1==k)
-  return root->val;
-prev++;
-if(root->right) {
-  int rightAns = KthSmallest(root->right, k);
-  if(rightAns)
-    return rightAns;
-}
-return -1;
+    int kthSmallest(TreeNode* root, int k) {
+
+      if(root==NULL)
+        return -1;
+      if(root->left) {
+        int leftAns = kthSmallest(root->left, k);
+        if(leftAns!=-1)
+          return leftAns;
+      }
+      if(prev+1==k)
+        return root->val;
+      prev++;
+      if(root->right) {
+        int rightAns = kthSmallest(root->right, k);
+        if(rightAns!=-1)
+          return rightAns;
+      }
+      return -1;
+    }
+};
--- validate_binary_search_tree.cpp
+++ validate_binary_search_tree.cpp
@@ -1,11 +1,27 @@
-void helper(TreeNode* root, TreeNode* mn, TreeNode* mx) {
-  if(root=NULL)
-    return true;
-  if(mn!=NULL && root-val<=mn-val)
-    return false;
-  if(mx!=NULL && root-val>=mx-val)
-    return false;
-  return helper(root->left, mn, root) && helper(root->right, root, mx);
-}
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool helper(TreeNode* root, TreeNode* mn, TreeNode* mx) {
+      if(root==NULL)
+        return true;
+      if(mn!=NULL && root->val<=mn->val)
+        return false;
+      if(mx!=NULL && root->val>=mx->val)
+        return false;
+      return helper(root->left, mn, root) && helper(root->right, root, mx);
+    }
 
-return helper(root, NULL, NULL);
+    bool isValidBST(TreeNode* root) {
+      return helper(root, NULL, NULL);
+    }
+};
