0bfe7a876d83074b8fcb614038f3b171124a733e
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-09-08 12:35:51 +0600

    shortes routes 2 problem solved

---
+++ Shortest_Routes_II.cpp
@@ -0,0 +1,150 @@
+// Starting with the name of Almighty Allah
+// Practice is the only shortcut to improve
+
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+
+//==================== TYPE ALIASES ====================//
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using i8 = __int128_t;
+using ui8 = __uint128_t;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+template<typename T>
+using vc = vector<T>;
+
+using vi = vc<int>;
+using vl = vc<ll>;
+using vpi = vc<pii>;
+using vpl = vc<pll>;
+
+//==================== CONSTANTS ====================//
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1000000007;
+const ld PI = acosl(-1.0L);
+
+//==================== MACROS ====================//
+#define pb push_back
+#define eb emplace_back
+#define F first
+#define S second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define srt(v) sort(all(v))
+#define rsrt(v) sort(rall(v))
+#define rvs(v) reverse(all(v))
+#define sz(x) (int)((x).size())
+#define nl '\n'
+#define cinv(v) for(auto &x : (v)) cin >> x
+#define coutv(v) for(auto &x : (v)) cout << x << ' '; cout << nl
+#define coutvl(v) for(auto &x : (v)) cout << x << nl
+#define Yes cout << "YES" << nl
+#define No cout << "NO" << nl
+#define yes cout << "Yes" << nl
+#define no cout << "No" << nl
+
+//==================== FUNCTIONS ====================//
+template<typename T>
+T gcd(T a, T b) {
+  while (b) {
+    T t = a % b;
+    a = b;
+    b = t;
+  }
+  return a;
+}
+
+template<typename T>
+T lcm(T a, T b) {
+  return a / gcd(a, b) * b;
+}
+
+template<typename T>
+bool ckmin(T &a, T b) {
+  if (b < a) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+template<typename T>
+bool ckmax(T &a, T b) {
+  if (b > a) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+//==================== RANDOM ====================//
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+int my_rand(int l, int r) {
+  return uniform_int_distribution<int>(l, r)(rng);
+}
+
+int n, e, q;
+ll adj_mat[501][501];
+
+void floyd_warshall() {
+  for (int k = 1; k <= n; k++) {
+    for (int i = 1; i <= n; i++) {
+      for(int j = 1; j <= n; j++) {
+        if(adj_mat[i][k]!=LINF && adj_mat[k][j]!=LINF && adj_mat[i][j]>adj_mat[i][k] + adj_mat[k][j])
+          adj_mat[i][j] = adj_mat[i][k] + adj_mat[k][j];
+      }
+    }
+  }
+}
+
+void solve() {
+  cin >> n >> e >> q;
+
+  for(int i =1; i <= n; i++) {
+    for(int j = 1; j <= n; j++) {
+      if(i==j)
+        adj_mat[i][j] = 0;
+      else
+        adj_mat[i][j] = LINF;
+    }
+  }
+
+  while(e--) {
+    ll a, b, c;
+    cin >> a >> b >> c;
+    adj_mat[a][b] = min(adj_mat[a][b],c);
+    adj_mat[b][a] = min(adj_mat[b][a],c);
+  }
+
+  floyd_warshall();
+
+  while(q--) {
+    int i, j;
+    cin >> i >> j;
+    if(adj_mat[i][j]==LINF)
+      cout << -1 << nl;
+    else 
+      cout << adj_mat[i][j] << nl;
+  }
+}
+
+int32_t main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t = 1;
+  // cin >> t;
+
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}
