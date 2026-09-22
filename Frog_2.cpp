2e6052fa14c6d8bb92215c138dafe11eaea81483
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-09-22 21:01:34 +0600

    frog 2 problem solved using recursive dp

--- Frog_2.cpp
+++ Frog_2.cpp
@@ -90,33 +90,30 @@ int my_rand(int l, int r) {
   return uniform_int_distribution<int>(l, r)(rng);
 }
 
-vector<int> dp(100005,-1);
+vector<int> dp(100004, -1);
 
-int minCost(int i, int n, vector<int> &v)  {
+int minCost(int i, int k, int n, vector<int> &v) {
   if(i>=n)
     return 0;
   if(i==n-1)
     return abs(v[i] - v[i + 1]);
   if(dp[i]!=-1)
     return dp[i];
-  return dp[i] = min(abs(v[i] - v[i + 1]) + minCost(i + 1, n, v), abs(v[i] - v[i + 2]) + minCost(i + 2, n, v));
+  int mn = INT_MAX;
+  for (int j = i+1; j <= min(i+k, n); j++) {
+    mn = min(mn, abs(v[i] - v[j]) + minCost(j, k, n, v));
+  }
+  return dp[i] = mn;
 }
 
 void solve() {
-  int n;
-  cin >> n;
+  int n, k;
+  cin >> n >> k;
   vi v(n + 1);
-  for (int i = 1; i <= n; i++)
+  for (int i = 1; i <= n; i++) {
     cin >> v[i];
-  // dp[1] = 0;
-  // dp[2] = abs(v[2]-v[1]);
-  // for (int i = 3; i<=n; i++) {
-  //   int cost1 = dp[i - 1] + abs(v[i - 1] - v[i]);
-  //   int cost2 = dp[i - 2] + abs(v[i - 2] - v[i]);
-  //   dp[i] = min(cost1, cost2);
-  // }
-  // cout << dp[n] << nl;
-  cout << minCost(1, n, v) << nl;
+  }
+  cout << minCost(1, k, n, v) << nl;
 }
 
 int32_t main() {
