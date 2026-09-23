c3dda7f568f56a582903ece7be0b2803b750522b
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-09-24 00:57:30 +0600

    labyrinth problem solved

--- Labyrinth.cpp
+++ Labyrinth.cpp
@@ -90,8 +90,78 @@ int my_rand(int l, int r) {
   return uniform_int_distribution<int>(l, r)(rng);
 }
 
+int n, m;
+char grid[1001][1001];
+bool vis[1001][1001];
+char parent[1001][1001];
+vector<pair<int, int>> moves = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+char dir[] = {'D', 'U', 'R', 'L'};
+
+bool valid(int ci, int cj) {
+  return ci >= 0 && ci < n && cj >= 0 && cj < m;
+}
+
+void BFS(int si, int sj) {
+  queue<pair<int, int>> q;
+  q.push({si, sj});
+  vis[si][sj] = true;
+  while(q.size()) {
+    auto par = q.front();
+    q.pop();
+    int pi = par.first;
+    int pj = par.second;
+    for (int i = 0; i < 4; i++) {
+      int ci = pi + moves[i].first;
+      int cj = pj + moves[i].second;
+      if(valid(ci,cj) && !vis[ci][cj] && grid[ci][cj]!='#') {
+        q.push({ci, cj});
+        vis[ci][cj] = true;
+        parent[ci][cj] = dir[i];
+      }
+    }
+  }
+}
+
 void solve() {
-  
+  int si = -1, sj = -1, di = -1, dj = -1;
+  cin >> n >> m;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      cin >> grid[i][j];
+      if(grid[i][j]=='A')
+        si = i, sj = j;
+      if(grid[i][j]=='B')
+        di = i, dj = j;
+    }
+  }
+  if(si==-1 || di ==-1) {
+    No;
+    return;
+  }
+  memset(vis, false, sizeof(vis));
+  memset(parent, 0, sizeof(parent));
+  BFS(si, sj);
+  if(vis[di][dj]) {
+    Yes;
+    string ans;
+    int ci = di, cj = dj;
+    while(ci!=si || cj!=sj) {
+      ans += parent[ci][cj];
+      if(parent[ci][cj]=='D')
+        ci--;
+      else if(parent[ci][cj]=='U')
+        ci++;
+      else if(parent[ci][cj]=='R')
+        cj--;
+      else if(parent[ci][cj]=='L')
+        cj++;
+    }
+    reverse(ans.begin(), ans.end());
+    cout << ans.size() << nl;
+    cout << ans << nl;
+  } else {
+    No;
+  }
 }
 
 int32_t main() {
