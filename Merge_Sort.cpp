5dd3c618c6fdfbcb4837a36eb2cb007ab30f8cd9
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-09-23 11:45:32 +0600

    merge sort problem solved in aizu online judge

---
+++ Merge_Sort.cpp
@@ -0,0 +1,156 @@
+// Starting with the name of Almighty Allah
+// Practice is the only shortcut to improve
+
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+
+//==================== TYPE ALIASES ====================//
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using i8 = __int128_t;
+using ui8 = __uint128_t;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+template<typename T>
+using vc = vector<T>;
+
+using vi = vc<int>;
+using vl = vc<ll>;
+using vpi = vc<pii>;
+using vpl = vc<pll>;
+
+//==================== CONSTANTS ====================//
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1000000007;
+const ld PI = acosl(-1.0L);
+
+//==================== MACROS ====================//
+#define pb push_back
+#define eb emplace_back
+#define F first
+#define S second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define srt(v) sort(all(v))
+#define rsrt(v) sort(rall(v))
+#define rvs(v) reverse(all(v))
+#define sz(x) (int)((x).size())
+#define nl '\n'
+#define cinv(v) for(auto &x : (v)) cin >> x
+#define coutv(v) for(auto &x : (v)) cout << x << ' '; cout << nl
+#define coutvl(v) for(auto &x : (v)) cout << x << nl
+#define Yes cout << "YES" << nl
+#define No cout << "NO" << nl
+#define yes cout << "Yes" << nl
+#define no cout << "No" << nl
+
+//==================== FUNCTIONS ====================//
+template<typename T>
+T gcd(T a, T b) {
+  while (b) {
+    T t = a % b;
+    a = b;
+    b = t;
+  }
+  return a;
+}
+
+template<typename T>
+T lcm(T a, T b) {
+  return a / gcd(a, b) * b;
+}
+
+template<typename T>
+bool ckmin(T &a, T b) {
+  if (b < a) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+template<typename T>
+bool ckmax(T &a, T b) {
+  if (b > a) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+//==================== RANDOM ====================//
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+int my_rand(int l, int r) {
+  return uniform_int_distribution<int>(l, r)(rng);
+}
+
+int cnt = 0;
+
+void merge(int l, int mid, int r, vector<int>& v) {
+  vector<int> tmp;
+  int i = l, j = mid + 1;
+  while(i<=mid && j<=r) {
+    if(v[i]<=v[j]) {
+      tmp.pb(v[i]);
+      i++;
+    } else {
+      tmp.pb(v[j]);
+      j++;
+    }
+    cnt++;
+  }
+  while(i<=mid) {
+    tmp.pb(v[i]);
+    i++;
+    cnt++;
+  }
+  while(j<=r) {
+    tmp.pb(v[j]);
+    j++;
+    cnt++;
+  }
+  for (int k = 0; k < sz(tmp); k++) {
+    v[k + l] = tmp[k];
+  }
+}
+
+void divide(int l, int r, vector<int>& v){
+  if(l<r) {
+    int mid = l + (r - l) / 2;
+    divide(l, mid, v);
+    divide(mid + 1, r, v);
+    merge(l, mid, r, v);
+  }
+}
+
+void solve() {
+  int n;
+  cin >> n;
+  vi v(n);
+  cinv(v);
+  divide(0, n - 1, v);
+  for (int i = 0; i < n-1; i++)
+    cout << v[i] << " ";
+  cout << v[n - 1] << nl;
+  cout << cnt << nl;
+}
+
+int32_t main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t = 1;
+  // cin >> t;
+
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}
