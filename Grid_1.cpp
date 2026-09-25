5d8f6204fa0dc6d310ca74cd186a8d1d6e0056d7
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-09-25 08:48:15 +0600

    grid 1 problem solved using recursion and dp

--- Grid_1.cpp
+++ Grid_1.cpp
@@ -90,11 +90,24 @@ int my_rand(int l, int r) {
   return uniform_int_distribution<int>(l, r)(rng);
 }
 
+int n, m;
+char grid[1001][1001];
+int dp[1001][1001];
+
+ll rec(int i, int j) {
+  if(i==n-1 && j ==m-1 )
+    return 1;
+  if(i>=n || j >=m)
+    return 0;
+  if(grid[i][j]=='#')
+    return 0;
+  if(dp[i][j] != -1)
+    return dp[i][j];
+  return dp[i][j] = (rec(i + 1, j)%MOD + rec(i, j + 1)%MOD) % MOD;
+}
 
 void solve() {
-  int n, m;
   cin >> n >> m;
-  char grid[n][m];
 
   for(int i = 0; i < n; i++) {
     for (int j = 0; j < m; j++)
@@ -102,28 +115,8 @@ void solve() {
       cin >> grid[i][j];
     }
   }
-  
-  vector<vector<ll>> dp(n, vector<ll>(m, 0));
-
-  dp[0][0] = 1;
-
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
-      if(grid[i][j]=='#') {
-        dp[i][j] = 0;
-        continue;
-      }
-      if(i>0) {
-        dp[i][j] += dp[i - 1][j];
-      }
-      if(j>0) {
-        dp[i][j] += dp[i][j - 1];
-      }
-      dp[i][j] %= MOD;
-    }
-  }
-
-  cout << dp[n - 1][m - 1] << nl;
+  memset(dp, -1, sizeof(dp));
+  cout << rec(0, 0);
 }
 
 int32_t main() {
